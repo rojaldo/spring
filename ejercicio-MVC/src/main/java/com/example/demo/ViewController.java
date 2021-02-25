2c9747d7d12f9f06d203390370533f927bf8b205
Daniel Rojo <danielrojo@gmail.com>
2021-02-25 14:11:55 +0100

    ejercicio_MVC for user form

--- ejercicio-MVC/src/main/java/com/example/demo/ViewController.java
+++ ejercicio-MVC/src/main/java/com/example/demo/ViewController.java
@@ -4,6 +4,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.validation.BindingResult;
+
+
 
 @Controller
 public class ViewController {
@@ -23,4 +29,17 @@ public class ViewController {
         model.addAttribute("users", repository.findAll());
         return "users.html";
     }
+
+    @GetMapping("/new_user_form")
+    public String users(Model model) {
+        model.addAttribute("user", new User());
+        return "newuserform.html";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveUser(@ModelAttribute User user, BindingResult errors, Model model) {
+        repository.save(user);
+        return "redirect:/users";
+    }
+
 }
